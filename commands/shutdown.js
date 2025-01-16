0e5f92776f34bdf7fda3a85196ccdfaf4bfd0f67
AirWan <45332400+AirWanFR@users.noreply.github.com>
2025-01-16 23:41:09 +0100

    Create shutdown.js

---
+++ commands/shutdown.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: '!shutdown',
+  execute(message) {
+    message.reply('Le bot se déconnecte et arrête le serveur...');
+    console.log('[INFO] Déconnexion en cours...');
+    
+    // Déconnecter le bot et arrêter le processus
+    message.client.destroy().then(() => {
+      console.log('[INFO] Bot déconnecté.');
+      process.exit(); // Arrêter le processus Node.js
+    }).catch((error) => {
+      console.log('[ERROR] Une erreur est survenue lors de la déconnexion :', error);
+    });
+  },
+};
