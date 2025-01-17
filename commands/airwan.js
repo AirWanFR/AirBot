155b222f238175b0b61fe733f8bac8232746114b
AirWan <45332400+AirWanFR@users.noreply.github.com>
2025-01-17 19:36:46 +0100

    Update airwan.js

--- commands/airwan.js
+++ commands/airwan.js
@@ -1,6 +1,24 @@
 module.exports = {
   name: '!airwan',
   execute(message) {
-    message.reply("Si vous avez besoin de plus d'informations, allez MP <@1282797720934813838>");
+      const exampleEmbed = new EmbedBuilder()
+  	.setColor(0x0099FF)
+  	.setTitle('Some title')
+  	.setURL('https://discord.js.org/')
+  	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+  	.setDescription('Some description here')
+  	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+  	.addFields(
+  		{ name: 'Regular field title', value: 'Some value here' },
+  		{ name: '\u200B', value: '\u200B' },
+  		{ name: 'Inline field title', value: 'Some value here', inline: true },
+  		{ name: 'Inline field title', value: 'Some value here', inline: true },
+  	)
+  	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+  	.setImage('https://i.imgur.com/AfFp7pu.png')
+  	.setTimestamp()
+  	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+  
+  channel.send({ embeds: [exampleEmbed] });
   },
 };
