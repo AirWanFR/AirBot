6c785d598531d89736de704f86779ad545bef939
AirWan <45332400+AirWanFR@users.noreply.github.com>
2025-01-18 22:58:31 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,16 @@
-const { EmbedBuilder } = require('discord.js');
-const dotenv = require('dotenv');
-dotenv.config();
-
-// Récupérer le préfixe depuis le fichier .env
-const PREFIX = process.env.PREFIX || "!";
+const { EmbedBuilder } = require('discord.js'); // Assurez-vous d'importer EmbedBuilder
 
 module.exports = {
   name: 'help',
-  description: 'Affiche la liste des commandes disponibles',
+  description: 'Affiche la liste des commandes disponibles.',
   execute(message) {
+    const PREFIX = '!'; // Définir ou récupérer ton préfixe ici
+
     // Créer un embed pour la commande help
     const helpEmbed = new EmbedBuilder()
-      .setColor(0x0099FF)
+      .setColor(0x0099FF) // Couleur de l'embed
       .setTitle('Liste des commandes disponibles')
-      .setDescription('Voici la liste des commandes que vous pouvez utiliser avec le bot :')
+      .setDescription('Voici la liste des commandes que vous pouvez utiliser avec ce bot :')
       .addFields(
         { name: `${PREFIX}airwan`, value: 'Affiche des informations sur Airwan.', inline: false },
         { name: `${PREFIX}infouser`, value: 'Affiche des informations sur l\'utilisateur.', inline: false },
@@ -25,8 +22,8 @@ module.exports = {
         { name: `${PREFIX}status`, value: 'Affiche le statut actuel du bot.', inline: false },
         { name: `${PREFIX}ping`, value: 'Affiche le ping actuel du bot.', inline: false }
       )
-      .setTimestamp()
-      .setFooter({ text: `Commande exécutée par ${message.author.username}`, iconURL: message.author.displayAvatarURL() });
+      .setTimestamp() // Ajouter un timestamp de l'exécution
+      .setFooter({ text: `Commande exécutée par ${message.author.username}`, iconURL: message.author.displayAvatarURL() }); // Ajouter un footer avec l'avatar de l'utilisateur
 
     // Envoyer l'embed dans le canal d'où provient la commande
     message.channel.send({ embeds: [helpEmbed] });
