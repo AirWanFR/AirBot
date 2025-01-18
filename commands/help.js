6957c228554370a04b2803083968d6cb74837c1b
AirWan <45332400+AirWanFR@users.noreply.github.com>
2025-01-18 22:11:24 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,14 +9,15 @@ module.exports = {
       .setTitle('Liste des commandes disponibles')
       .setDescription('Voici les commandes que vous pouvez utiliser avec le bot.')
       .addFields(
-        { name: '!airwan', value: 'Affiche des informations sur le bot.', inline: false },
-        { name: '!infouser', value: 'Affiche les informations d\'un utilisateur.', inline: false },
-        { name: '!presence', value: 'Change la présence du bot.', inline: false },
-        { name: '!setactivity', value: 'Change l\'activité du bot.', inline: false },
-        { name: '!setstatus', value: 'Change le statut du bot (online, idle, dnd).', inline: false },
-        { name: '!shutdown', value: 'Arrête le bot.', inline: false },
-        { name: '!status', value: 'Affiche l\'état actuel du bot.', inline: false },
-        { name: '!help', value: 'Affiche cette aide.', inline: false },
+        { name: `${PREFIX}airwan`, value: 'Affiche des informations sur le bot.', inline: false },
+        { name: `${PREFIX}infouser`, value: 'Affiche les informations d\'un utilisateur.', inline: false },
+        { name: `${PREFIX}presence`, value: 'Change la présence du bot.', inline: false },
+        { name: `${PREFIX}setactivity`, value: 'Change l\'activité du bot.', inline: false },
+        { name: `${PREFIX}setstatus`, value: 'Change le statut du bot (online, idle, dnd).', inline: false },
+        { name: `${PREFIX}shutdown`, value: 'Arrête le bot.', inline: false },
+        { name: `${PREFIX}status`, value: 'Affiche l\'état actuel du bot.', inline: false },
+        { name: `${PREFIX}help`, value: 'Affiche cette aide.', inline: false },
+        { name: `${PREFIX}setprefix`, value: 'Modifie le préfixe du bot.', inline: false },
       )
       .setFooter({ text: `Commande exécutée par ${message.author.username}`, iconURL: message.author.displayAvatarURL() })
       .setTimestamp();
